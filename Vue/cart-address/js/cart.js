16cad5a8fed126654fd0917f63aa09f6a14359d5
zhoujing0519 <403502303@qq.com>
2017-08-08 17:53:38 +0800

    cart

--- Vue/cart-address/js/cart.js
+++ Vue/cart-address/js/cart.js
@@ -3,20 +3,57 @@ var app = new Vue({
     data: {
         totalMoney: 0,
         productList: [],
+        checkAllFlag: false,
     },
     filters: {
-
+        formatCurrency: function(value){
+            return '￥' + value.toFixed(2);
+        },
     },
     mounted: function(){
         this.cartView();
     },
     methods: {
         cartView: function(){
-            var self = this;
-            this.$http.get('data/cartData.json').then(function(res){
-                self.productList = res.data.result.list;
-                self.totalMoney = res.data.result.totalMoney;
+            this.$http.get('data/cartData.json').then(res => {
+                this.productList = res.data.result.list;
+                this.totalMoney = res.data.result.totalMoney;
             });
         },
+        changeCount: function(product, type){
+            if(type > 0){
+                product.productQuantity++;
+            }else{
+                if(product.productQuantity === 1) return;
+                product.productQuantity--;
+            }
+        },
+        selectProduct: function(item){
+            if(typeof item.checked == 'undefined'){
+                // Vue.set(item, 'checked', true);
+                this.$set(item, 'checked', true);
+            }else{
+                item.checked = !item.checked;
+            }
+        },
+        checkAll: function(flag){
+            let self = this;
+            this.checkAllFlag = flag;
+
+            if(this.checkAllFlag){
+                this.productList.forEach(function(item, index){
+                    if(typeof item.checked == 'undefined'){
+                        self.$set(item, 'checked', true);
+                    }else{
+                        item.checked = flag;
+                    }
+                });
+            }
+        },
     },
+});
+
+// 全局过滤器
+Vue.filter('money', function(value, type){
+    return '￥' + value.toFixed(2) + type;
 });
